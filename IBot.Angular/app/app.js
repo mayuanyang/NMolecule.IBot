71985ce7a1cc188486b18f931d9702142ffa9671
Eddy <ocean_ltd@hotmail.com>
2016-07-26 21:43:37 +1000

    Remove unuse dependancy

--- IBot.Angular/app/app.js
+++ IBot.Angular/app/app.js
@@ -3,12 +3,8 @@
 import traceur from './traceur-runtime';
 import router from './angular-ui-router';
 import angularBusy from './angular-busy';
-import angularScroll from './angular-scroll';
 import httpService from './httpService';
 import guidFactory from './guidFactory';
-import angularToggleSwitch from './angular-toggle-switch'; 
-import chart from './chart.js'; 
-import angularchart from './angular-chart.js';
 import uiboot from './ui-bootstrap-tpls';
 import homeLayoutController from './homeLayoutController';
 
@@ -17,7 +13,7 @@ import loadDirective from './loadDirective';
 import enterPress from './enterPress';
 
 console.log("version: " + angular.version.full);
-var myApp = angular.module('myApp', ['ui.router', 'cgBusy', 'duScroll', 'toggle-switch','ui.bootstrap', 'dynamicTemplateDirective', 'loadDynamicTemplateDirective', 'enterPressDirective'])
+var myApp = angular.module('myApp', ['ui.router', 'cgBusy', 'ui.bootstrap', 'dynamicTemplateDirective', 'loadDynamicTemplateDirective', 'enterPressDirective'])
 	.controller('homeLayoutController', homeLayoutController)
 	.factory('guidFactory', guidFactory.createFactory)
 	.service('httpService', httpService);
